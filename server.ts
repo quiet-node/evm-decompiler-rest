4f2c1982b3399dc927717a8b8e29c4d8e3325f00
Logan Nguyen <logan.nguyen@swirldslabs.com>
2023-11-29 00:55:19 -0600

    pkg: added cors package
    Signed-off-by: Logan Nguyen <logan.nguyen@swirldslabs.com>

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
 import decompilerRouter from './routes/api/decompile';
 import { sanitizeBytecodeMiddlware } from './middleware/sanitize-bytecode';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/decompile', sanitizeBytecodeMiddlware, decompilerRouter);
 
